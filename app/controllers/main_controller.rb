ae19dcca429f2f34e70dcce866e58b446dd51936
DragonMaster <ms235389@gmail.com>
2021-06-27 12:17:11 +0530

    add styles/bootstrap

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
 
     def index
-        
+        flash[notice] = "Logged in Successfully"
+        flash[alert] = "Invalid email or password"
     end
 
 end    
